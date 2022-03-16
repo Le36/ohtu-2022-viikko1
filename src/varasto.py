8779d5648a21103095cdeec66f2ba3ce5996889d
Le36 <48809753+Le36@users.noreply.github.com>
2022-03-16 13:33:00 +0200

    fix tests

--- src/varasto.py
+++ src/varasto.py
@@ -1,11 +1,11 @@
 class Varasto:
-    def __init__(self, tilavuus, alku_saldo = 0):
+    def __init__(self, tilavuus, alku_saldo=0):
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        
+
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -28,7 +28,18 @@ class Varasto:
         else:
             self.saldo = self.tilavuus
 
+    def ota_varastosta(self, maara):
+        if maara < 0:
+            return 0.0
+        if maara > self.saldo:
+            kaikki_mita_voidaan = self.saldo
+            self.saldo = 0.0
 
-    
+            return kaikki_mita_voidaan
+
+        self.saldo = self.saldo - maara
+
+        return maara
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
