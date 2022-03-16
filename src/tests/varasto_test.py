c9426ce00761a08e7603b1133c13da0d493af223
Le36 <48809753+Le36@users.noreply.github.com>
2022-03-16 12:56:29 +0200

    add missing tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,28 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_on_taysi(self):
+        self.varasto.lisaa_varastoon(9)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 1)
+        self.varasto.lisaa_varastoon(5)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_ota_liikaa(self):
+        self.varasto.ota_varastosta(50)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisaa_negatiivinen(self):
+        self.varasto.lisaa_varastoon(-5)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_ota_negatiivinen(self):
+        self.varasto.ota_varastosta(-5)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_konstruktori(self):
+        self.var = Varasto(-5, -5)
+        self.varr = Varasto(2, 3)
+
+    def test_print(self):
+        self.assertAlmostEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
